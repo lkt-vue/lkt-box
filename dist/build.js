c64c72847688b6bb121a7ab87197159760b6aa57
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2024-04-05 17:13:16 +0200

    Supports class and style props

--- dist/build.js
+++ dist/build.js
@@ -1,50 +1,31 @@
-var __defProp = Object.defineProperty;
-var __defProps = Object.defineProperties;
-var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
-var __getOwnPropSymbols = Object.getOwnPropertySymbols;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __propIsEnum = Object.prototype.propertyIsEnumerable;
-var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __spreadValues = (a, b) => {
-  for (var prop in b || (b = {}))
-    if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
-  if (__getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
-    }
-  return a;
-};
-var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
-import { defineComponent, computed, openBlock, createElementBlock, normalizeClass, renderSlot } from "vue";
-const __default__ = {
-  name: "LktBox",
-  inheritAttrs: false,
-  customOptions: {}
-};
-const _sfc_main = /* @__PURE__ */ defineComponent(__spreadProps(__spreadValues({}, __default__), {
+import { defineComponent, computed, openBlock, createElementBlock, normalizeClass, normalizeStyle, renderSlot } from "vue";
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  __name: "LktBox",
   props: {
-    palette: String
+    palette: { default: "" },
+    style: { default: "" },
+    class: { default: "" }
   },
   setup(__props) {
     const props = __props;
     const classes = computed(() => {
       let r = ["lkt-box"];
-      if (props.palette) {
+      if (props.palette)
         r.push(`lkt-box--${props.palette}`, `palette-${props.palette}`);
-      }
+      if (props.class)
+        r.push(props.class);
       return r.join(" ");
     });
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock("section", {
-        class: normalizeClass(classes.value)
+        class: normalizeClass(classes.value),
+        style: normalizeStyle(_ctx.style)
       }, [
         renderSlot(_ctx.$slots, "default")
-      ], 2);
+      ], 6);
     };
   }
-}));
+});
 const LktBoxPlugin = {
   install: (app, options) => {
     app.component("lkt-box", _sfc_main);
--- src/lib-components/LktBox.vue
+++ src/lib-components/LktBox.vue
@@ -1,30 +1,27 @@
-<script lang="ts">
-export default {
-    name: "LktBox",
-    inheritAttrs: false,
-    customOptions: {}
-}
-</script>
-
 <script setup lang="ts">
 import {computed} from 'vue';
 
-const props = defineProps({
-    palette: String
-})
+const props = withDefaults(defineProps<{
+    palette: string
+    style: string
+    class: string
+}>(), {
+    palette: '',
+    style: '',
+    class: '',
+});
 
 const classes = computed(() => {
     let r = ['lkt-box'];
 
-    if (props.palette) {
-        r.push(`lkt-box--${props.palette}`, `palette-${props.palette}`);
-    }
+    if (props.palette) r.push(`lkt-box--${props.palette}`, `palette-${props.palette}`);
+    if (props.class) r.push(props.class);
     return r.join(' ');
-})
+});
 </script>
 
 <template>
-    <section v-bind:class="classes">
+    <section v-bind:class="classes" :style="style">
         <slot name="default"></slot>
     </section>
 </template>
